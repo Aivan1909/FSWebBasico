2399186bc655f3d2de344a29f3611604929b5e71
Aivan <aivatepaz@gmail.com>
2020-04-07 12:39:21 -0400

    Se agrego Sesion 2 de HTML y modificado del modal

--- scripts/diapos.js
+++ scripts/diapos.js
@@ -14,10 +14,19 @@ var diapo = [
         },
         {
             titulo: "SESION 2",
+            fecha: "07/04/2020",
+            subtitulo: "",
+            imagen: "https://image.freepik.com/vector-gratis/fondo-mano-rellenando-formulario_23-2147607013.jpg",
+            contenido: "",
+            lista:["Imagenes", "Audio", "Video", "Tablas", "Formularios"],
+            descarga: "descargas/HTML_Sesion_2.rar"
+        },
+        {
+            titulo: "SESION 3",
             fecha: "",
             subtitulo: "Material aun no subido",
             imagen: "",
-            contenido: "Proxima Clase el Viernes 3 de Abril de 10:00AM a 12:00PM. Nos vemos ese dia... ;v",
+            contenido: "Proxima Clase el Viernes 10 de Abril de 10:00AM a 12:00PM. Nos vemos ese dia... ;v",
             lista:[],
             descarga: ""
         }
